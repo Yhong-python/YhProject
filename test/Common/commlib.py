424f1fc2df7ec3ddba38d5a465c9ddcbb5fbb9dc
yanghong <hong.yang@qizhixinxi.com>
2020-07-29 15:58:07 +0800

    增加从用例读取数据后进行数据转化的方法

--- test/Common/commlib.py
+++ test/Common/commlib.py
@@ -6,10 +6,15 @@
 @time: 2020/7/22 15:03
 @desc:
 '''
-import jsonpath
-from ruamel import yaml
 import json
+
+import requests
+from ruamel import yaml
+
 from test.Common import global_param
+from test.Common.Log import Log
+
+log = Log().getlog()
 
 def get_test_data(test_data_path, test_node):
     case = []  # 存储测试用例名称
@@ -32,11 +37,47 @@ def get_test_data(test_data_path, test_node):
     return case, parameters
 
 
-def get_requests_data(data):
-    for key, value in data.items():
-        print(key, value)
-        # if value.startswith("$") and value.endswith("$"):
-        #     paramData=global_param.ALL_PARAM['companyList']
+class Convert:
+    def convert_value(self, value):
+        """
+        :param value: 值
+        :return: 返回一个转化类型后的值
+        """
+        if isinstance(value, int):  # int类型的直接返回
+            return value
+        elif isinstance(value, str):
+            # 选判断需要转化的数据中是否存在$x$的引用，存在的话先替换数据
+            if value.startswith("$") and value.endswith("$"):
+                global_key = value.split("$")[1]  # 取删除标识符号后的数据
+                if global_key:  # 不为空时
+                    global_value = global_param.ALL_PARAM.get(global_key, None)
+                    if global_value:  # 在保存的全局变量中，该变量不为空时替换数据
+                        log.info("进行全局变量替换，{}替换为{}".format(value, global_value))
+                        value = global_value
+                        return json.dumps(value)
+                    else:
+                        log.error("在global_param.ALL_PARAM保存的全局变量中未找到该key:{}对应的非空数值".format(global_key))
+                        raise ValueError("在global_param.ALL_PARAM保存的全局变量中未找到该key:{}对应的非空数值".format(global_key))
+                else:
+                    log.error("引用全局变量失败，请检查：{}的格式是否符合要求。正确格式例子：$KEY_NAME$".format(value))
+                    raise KeyError("引用全局变量失败，请检查：{}的格式是否符合要求。正确格式例子：$KEY_NAME$".format(value))
+            else:  # 不需要进行全局变量替换的
+                return value
+        elif isinstance(value, (list, tuple)):  # list/tuple类型的数据转化成str
+            return json.dumps(value)
+        elif isinstance(value, dict):  # dict类型的数据转化成str
+            return json.dumps(value)
+        else:
+            return value
+
+    def getReqData(self, casedata):
+        if isinstance(casedata, dict):
+            for key in casedata:
+                casedata[key] = self.convert_value(casedata[key])
+            return casedata
+        else:
+            raise TypeError("传入的数据类型必须为dict,当前传入的数据类型为{}".format(type(casedata)))
+
 
 if __name__ == "__main__":
     data = get_test_data('../TestCaseData/organizeManage.yml', 'test2')
@@ -60,3 +101,11 @@ if __name__ == "__main__":
 
     # print(data[1])
 
+    global_param.ALL_PARAM['COMPLIST'] = {"data": {"asa": 1}}
+
+    data = get_test_data('../TestCaseData/organizeManage.yml', "tests1")
+    a = Convert()
+    for casedata in data[1]:
+        reqData = a.getReqData(casedata[1]['data'])
+        print(reqData)
+        r = requests.post("https://www.baidu.com", data=reqData, verify=False)
