aabce6072cf784ffecd68ceeaf5d07484b15dfef
yanghong <hong.yang@qizhixinxi.com>
2021-03-10 13:57:41 +0800

    增加单例模式的LOG文件

---
+++ test/Common/Log_single.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: yanghong
+@file: Log.py
+@time: 2020/5/12 15:57
+@desc:日志打印封装，不用这个
+'''
+import logging
+import os
+import time
+import threading
+
+# log_path是存放日志的路径
+cur_path = os.path.dirname(__file__)
+log_path = os.path.join(os.path.dirname(cur_path), 'Log')
+# 如果不存在logs文件夹则创建一个
+if not os.path.exists(log_path): os.mkdir(log_path)
+
+
+class Log:
+    _instance_lock=threading.Lock()
+    def __init__(self, logger=None):
+        # 文件的命名
+        self.logname = os.path.join(log_path, '%s.log' % (time.strftime('%Y%m%d')))  # 全部日志文件名
+        self.error_logname = os.path.join(log_path, '%s_error.log' % (time.strftime('%Y%m%d')))  # 错误日志文件名
+
+        self.logger = logging.getLogger(logger)
+        self.logger.handlers.clear()
+        self.logger.setLevel(level=logging.INFO)
+        # 日志输出格式
+        self.formatter = logging.Formatter(
+            fmt='%(asctime)s %(filename)s -> %(funcName)s line:%(lineno)d [%(levelname)s] %(message)s')
+        # 创建filehandle，用于写全部日志到文件
+        self.filehandle = logging.FileHandler(self.logname, encoding='utf-8')
+        self.filehandle.setLevel(level=logging.DEBUG)
+        self.filehandle.formatter = self.formatter
+        self.logger.addHandler(self.filehandle)
+
+        # # 创建filehandle，用于写错误日志到文件
+        # filehandle_error = logging.FileHandler(self.error_logname, encoding='utf-8')
+        # filehandle_error.setLevel(level=logging.ERROR)
+        # filehandle_error.formatter = self.formatter  # 格式与全部日志的相同
+        # self.logger.addHandler(filehandle_error)
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls,"_instance"):
+            with cls._instance_lock:
+                if not hasattr(cls,"_instance"):
+                    cls._instance=object.__new__(cls)
+        return cls._instance
+    def getlog(self):
+        return self.logger
+
+    def info(self, log_meg):
+        logger = self.getlog()  # 获取打印日志时的文件名称及行号
+        logger.info(log_meg)
+
+    def debug(self, log_meg):
+        logger = self.getlog()
+        logger.debug(log_meg)
+
+    def warning(self, log_meg):
+        logger = self.getlog()
+        logger.warning(log_meg)
+
+    def error(self, log_meg):
+        logger = self.getlog()
+        logger.error(log_meg)
+
+    def exception(self, log_meg):
+        logger = self.getlog()
+        logger.exception(log_meg)
+
+    def critical(self, log_meg):
+        logger = self.getlog()
+        logger.critical(log_meg)
+
+
+if __name__ == "__main__":
+    log1 = Log()
+    print(id(log1))
+    # log = log1.getlog()
+    # log.info('--测试开始--')
+    # log.info('操作步骤1,2,3')
+    # log.warning('----测试结束----')
+    # log2 = Log()
+    # print(id(log2))
+    # print(log1 is log2)
+
+    log2 = Log()
+    print(id(log2))
+    # log.info('--测试开始--')
+    # log.info('操作步骤1,2,3')
+    # log.warning('----测试结束----')
+    # log2 = Log().getlog()
+    # print(id(log2))
+    # print(log1 is log2)
